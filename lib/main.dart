b3868055c1c82b5622ba9a59a0d27d8ac0c69f2b
surajsisodia <sisodiasuraj2000@gmail.com>
2022-03-07 23:27:52 +0530

    Visitor Counter Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/home.dart';
+import 'package:my_portfolio/utils/firebase_config.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: fAPIKey,
+          appId: fAppId,
+          messagingSenderId: fMessagingId,
+          projectId: fProjectId));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   final txtStl = TextStyle(fontFamily: "Poppins");
-  // This widget is the root of your application.
+
+  increaseVisitorCounter() {
+    FirebaseFirestore.instance
+        .collection('stats')
+        .doc('visitorCounter')
+        .update({"counter": FieldValue.increment(1)});
+  }
+
   @override
   Widget build(BuildContext context) {
+    increaseVisitorCounter();
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Suraj Sisodia',
